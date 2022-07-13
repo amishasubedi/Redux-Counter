2bdb23d9f9d53d43e292b76ab4567b27c923f8bb
amishasubedi <amisha.subedi1@gmail.com>
2022-07-13 01:19:39 -0500

    added state slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,40 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+/**
+ * createSlice() accepts initial state, an object of reducer functions, and a slice name
+ */
+createSlice({
+  name: "counter", // a name, used in action types
+  initialState, // initial state for reducer
+
+  // an object of key reducers
+  reducers: {
+    increment(state) {
+      state.counter++; // although it seems mutable, this is handled by inner redux toolkit package which keeps the existing state
+    },
+
+    decrement(state) {
+      state.counter--;
+    },
+
+    // multiple state
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
+      // NEVER MUTATE THE STATE like this (state.counter++) here because it will change the existing state
       counter: state.counter + 1,
       showCounter: state.showCounter,
     };
