9be77fe295201b33ce53b439e2bec7cec4c98c56
amishasubedi <amisha.subedi1@gmail.com>
2022-07-12 19:39:11 -0500

    dispatched actions from inside component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,29 @@
-import { useSelector, connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
-/**
- * extracting data from the Redux store state using useSelector() function
+/**-------------------------------------------useSelector()---------------------------------------------
+ * extracts data from the Redux store state
  * it takes the current state as an argument and returns value derived from that state
  * each time it runs, it returns a new object
  * so, returning a new object every time will always force a re-render by default
+ *
+ *
+ * --------------------------------------------useDispatch()---------------------------------------------
+ * returns a reference to the dispatch function from the Redux store
+ *
  */
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -17,6 +31,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
