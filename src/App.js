9b2e7a4cb3fd898112edc2dc860a5f3bb458b6aa
amishasubedi <amisha.subedi1@gmail.com>
2022-07-15 02:13:27 -0500

    dispatched actions from authentication slice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticates);
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
